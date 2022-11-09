4e6bb55171f462ee61a0606e16c06f8b0567b5f6
MakarovOleg1988 <makarov.oleg1988@gmail.com>
2022-11-09 17:10:02 +0530

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lesson33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] finalists = { "James Van", "John Smith", "Leyla Browm", "Jim Gordon", "Maksim Bren" };
+
+            int winner = Convert.ToInt32(Console.ReadLine());
+
+            FinalRound _finalRound = new FinalRound(finalists, winner);
+
+            GameOver _gameOver = new GameOver();
+        }
+    }
+
+    class FinalRound
+    {
+        public FinalRound(string[] finalists, int winner)
+        {
+            string personWinner = finalists[winner];
+            Console.WriteLine("Winner is" + " " + personWinner);
+        }
+    }
+
+    class GameOver
+    {
+        public GameOver()
+        {
+            Console.WriteLine("Game Over");
+        }
+        ~GameOver() { }
+    }
+}
+
+
+
